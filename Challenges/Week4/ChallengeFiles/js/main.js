2e140820d37585c2697e4bc827f455bd80283cf4
Amanda Free <amanda.free02@gmail.com>
2016-09-17 19:50:04 -0400

    Week 4

--- Challenges/Week4/ChallengeFiles/js/main.js
+++ Challenges/Week4/ChallengeFiles/js/main.js
@@ -6,14 +6,17 @@ function openBlock (id) {
         blocks[y].style.display = "none";
     }
     block.style.display = "block";
-    
-    // TODO: remove the "active" class from all of the li elements inside the menu
-    // TODO: add the "active" class to the li element that contains the link that was clicked
 
-}
+    $('.menu').on('click', 'li', function() {
+      // TODO: remove the "active" class from all of the li elements inside the menu
+      $('.menu li').removeClass('.active');
+      // TODO: add the "active" class to the li element that contains the link that was clicked
+      $(this).addClass('.active');
+    });
 
 // TODO: add the "hover" class to the menu items when you hover over them
-
+$('.menu').on('hover', 'li', function() {
+  $(this).addClass('.hover');
+});
 
 // TODO: set up the tooltip plugin on all of the links in the menu
-
